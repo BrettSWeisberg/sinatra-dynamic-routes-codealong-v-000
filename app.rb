b41c7dcd843d8f49748db0e57b6ddfaa4fe80d95
Brett Weisberg <weisberg.brett@gmail.com>
2018-06-19 00:33:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,10 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-
     @product_id_1 = params[:num1]
     @product_id_2 = params[:num2]
-    
-    "#{@product_id_1}, #{@product_id_2}"
+    "#{@product_id_1} * #{@product_id_2}"
   end
 end
