c382dafad6e48ab98521ec0977f7755b84bb4bbd
Brett Weisberg <weisberg.brett@gmail.com>
2018-06-19 00:25:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    binding.pry
+    
     @product_id_1 = params[:num1]
     @product_id_2 = params[:num2]
     "#{@product_id_1},#{@product_id_2}"
